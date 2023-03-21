c5cb678870c93661fc94ec19203a3330e0b5e8a4
RyuseiTaniguchi <taniguchi.ryusei@gmail.com>
2023-03-21 12:15:12 -0700

    add filter html css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
 import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import sampleRecipes from './sample_data'
+import Select from 'react-select'
+
 // import './App.css'
 
 // https://api.edamam.com/api/recipes/v2?type=public&q=potato&app_id=cc240aca&app_key=0a8c95e6cec1cf2d3976f987892f75d7
 
-const DEBUG = true
+const DEBUG = true;
 
-const API_ENDPORT = "https://api.edamam.com/api/recipes/v2?type=public&app_id=cc240aca&app_key=0a8c95e6cec1cf2d3976f987892f75d7&q="
+const API_ENDPORT = `https://api.edamam.com/api/recipes/v2?
+type=public&app_id=cc240aca&app_key=0a8c95e6cec1cf2d3976f987892f75d7&
+field=uri&field=url&field=source&field=calories&field=label&field=image&q=`;
+
 let next_page;
 
 const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(localStorage.getItem(key) || initialValue);
 
   useEffect(() => {
-    localStorage.setItem(key, storedValue)
+    localStorage.setItem(key, storedValue);
   }, [storedValue])
 
-  return [storedValue, setStoredValue]
+  return [storedValue, setStoredValue];
 }
 
 const load_recipe = async (url) => {
-  const data = await fetch(url)
-  const response = await data.json()
+  const data = await fetch(url);
+  const response = await data.json();
   try{
-    next_page = response._links.next.href
+    next_page = response._links.next.href;
   }catch{
-    next_page = null
+    next_page = null;
   }
-  return [response.hits, next_page]
+  return [response.hits, next_page];
 }
 
 
@@ -39,23 +44,26 @@ const App = () => {
 
   useEffect(() => {
     const load = async () => {
-      let [hits, next_page] = await load_recipe(API_ENDPORT + searchTerm)
-      setRecipes(hits)
+      let [hits, next_page] = await load_recipe(API_ENDPORT + searchTerm);
+      setRecipes(hits);
     }
-    if (!DEBUG) load()
+    if (!DEBUG) load();
   }, [searchTerm])
 
   return(
     <>
     <Nav setSearchTerm={setSearchTerm} searchingTerm={searchingTerm} setSearchingTerm={setSearchingTerm}/>
     <main>
-      <div className='sidebar'>
-        <h2>Sidebar</h2>
-      </div>
+      <h1>Result: "{searchTerm}"</h1>
 
-      <div className='main_content'>
-        <h1>Result: "{searchTerm}"</h1>
-        <RecipeList recipes={recipes} setRecipes={setRecipes}/>
+      <div id='result_content'>
+        <div className='sidebar'>
+          <Filters />
+        </div>
+
+        <div className='main_content'>
+          <RecipeList recipes={recipes} setRecipes={setRecipes}/>
+        </div>
       </div>
     </main>
     </>
@@ -87,6 +95,67 @@ const Nav = ({setSearchTerm, searchingTerm, setSearchingTerm}) => {
   )
 }
 
+const Filters = () => {
+
+  const mealType_options = [
+    { value: 'breakfast', label: 'Breakfast' },
+    { value: 'lunch', label: 'Lunch' },
+    { value: 'dinner', label: 'Dinner' },
+    { value: 'snack', label: 'Snack' },
+    { value: 'teatime', label: 'Teatime' }
+  ]
+
+  const dishType_options = [
+    {value: 'alcohol cocktail', label: 'Alcohol Cocktail'},
+    {value: 'biscuits and cookies', label: 'Biscuits and Cookies'},
+    {value: 'bread', label: 'Bread'},  {value: 'cereals', label: 'Cereals'},
+    {value: 'condiments and sauces', label: 'Condiments and Sauces'},
+    {value: 'desserts', label: 'Desserts'},  {value: 'drinks', label: 'Drinks'},
+    {value: 'egg', label: 'Egg'},  {value: 'ice cream and custard', label: 'Ice Cream and Custard'},
+    {value: 'main course', label: 'Main Course'},  {value: 'pancake', label: 'Pancake'},
+    {value: 'pasta', label: 'Pasta'},  {value: 'pastry', label: 'Pastry'},
+    {value: 'pies and tarts', label: 'Pies and Tarts'},  {value: 'pizza', label: 'Pizza'},
+    {value: 'preps', label: 'Preps'},  {value: 'preserve', label: 'Preserve'},
+    {value: 'salad', label: 'Salad'},  {value: 'sandwiches', label: 'Sandwiches'},
+    {value: 'seafood', label: 'Seafood'},  {value: 'side dish', label: 'Side Dish'},
+    {value: 'soup', label: 'Soup'},  {value: 'special occasions', label: 'Special Occasions'},
+    {value: 'starter', label: 'Starter'},  {value: 'sweets', label: 'Sweets'}
+  ]
+
+  return (
+    <div>
+      <h2>Filter</h2>
+      <div className='filters'>
+        <div className='filter'>
+          <h3>Calories Range</h3>
+          <div className='filter_content'>
+            <div className='num_range'>
+              <input type="number" placeholder='min' min={0} step={100} />
+              <input type="number" placeholder='max' max={5000} step={100}/>
+            </div>
+          </div>
+        </div>
+
+        <div className='filter'>
+          <h3>Meal Type</h3>
+          <div className='filter_content'>
+            <Select options={mealType_options} isMulti closeMenuOnSelect={false} onChange={(value)=> console.log(value)}/>
+          </div>
+        </div>
+
+        <div className='filter'>
+          <h3>Dish Type</h3>
+          <div className='filter_content'>
+            <Select options={dishType_options} isMulti closeMenuOnSelect={false} onChange={(value)=> console.log(value)}/>
+          </div>
+        </div>
+      </div>
+
+      <button id="apply_filter">Apply</button>
+    </div>
+)
+}
+
 const RecipeList = ({recipes, setRecipes}) => {
 
   const LoadMore = () => {
@@ -113,7 +182,7 @@ const RecipeBox = ({recipe}) => {
     <div className='recipe_box'>
       <a href={recipe.url} target="_blank">
         <div className='recipe_image_box'>
-          <img src={recipe.image} alt="vite logo" />
+          <img src={recipe.image} alt={recipe.label} />
           <p className='recipe_calories'>{Math.round(recipe.calories)} kcal</p>
         </div>
         <p className='recipe_title'>{recipe.label}</p>
